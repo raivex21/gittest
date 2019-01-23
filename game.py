76c257996c475f0765d6278b19e6fa57e84c955c
raivex21 <austinjohnxtegaran@gmail.com>
2019-01-23 08:05:16 +0800

    game.py with stats

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
 print("Welcome to Python Game!")
 
 
+player = {'player1': { 'name': 'Rai', 'full_hp': 25, 'current_hp': 25, 'full_mana': 20, 'current_mana': 20, 'physical_damage': 2, 'magical_damage': 2, 'armor': .5, 'spell_resist': .5, 'class': ''}}
+
+for i in player.items():
+	print(i)
